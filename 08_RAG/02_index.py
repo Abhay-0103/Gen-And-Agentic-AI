3d8f3673a04bb10098b6a4a183c77861deec4e25
Abhay-0103 <ab0321054@gmail.com>
2026-04-11 12:24:29 +0530

    chuckinh

---
+++ 08_RAG/02_index.py
@@ -0,0 +1,20 @@
+# This is for splitting into the chunks
+
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+ 
+
+pdf_path = Path(__file__).parent / "nodejs.pdf"
+
+# Load this file in python program
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+# Spliting the document into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400,
+)
+
+chunks = text_splitter.split_documents(documents=docs)
