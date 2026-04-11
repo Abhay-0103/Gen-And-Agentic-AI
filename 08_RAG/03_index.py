96a13bc78f843dff36cb9f58a79262d30c30c06c
Abhay-0103 <ab0321054@gmail.com>
2026-04-11 12:30:49 +0530

    used vectorDB and hugging face model

---
+++ 08_RAG/03_index.py
@@ -0,0 +1,43 @@
+# using VectorDB for indexing the documents
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+
+load_dotenv()
+
+pdf_path = Path(__file__).parent / "nodejs.pdf"
+
+# Load this file in python program
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+# Spliting the document into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400,
+)
+
+chunks = text_splitter.split_documents(documents=docs)
+
+# Creating VectorDB index for the chunks
+embeddings = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    url="http://localhost:6333",
+    collection_name="Learning_RAG",
+)
+
+print("Indexing of document Done....!")
